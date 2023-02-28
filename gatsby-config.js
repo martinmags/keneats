b0900c425a2d2c8d5ea3207dd8ec6011e275f0f6
Martin Magsombol <martinremags@gmail.com>
2023-02-28 06:49:40 -0800

    remove contentful for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,13 +6,15 @@ module.exports = {
     title: `Keneats`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "CFPAT-_sZx2kUzJuKhQvm2Ju5EFgdh7wwT2T2Z57KCHgwqxP8",
-      "spaceId": "5i2f86b9dc37"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-emotion", "gatsby-plugin-google-gtag", "gatsby-plugin-sitemap", {
+  plugins: [
+  // {
+  //   resolve: 'gatsby-source-contentful',
+  //   options: {
+  //     "accessToken": "CFPAT-_sZx2kUzJuKhQvm2Ju5EFgdh7wwT2T2Z57KCHgwqxP8",
+  //     "spaceId": "5i2f86b9dc37"
+  //   }
+  // }, 
+  "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-emotion", "gatsby-plugin-google-gtag", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
